ef435e26666827bf3f3aa88666345ee9931f3b34
ssommerr <143761525+ssommerr@users.noreply.github.com>
2023-11-23 18:49:17 -0300

    cambio 5

--- proyectoprincipal/principal/principal.js
+++ proyectoprincipal/principal/principal.js
@@ -23,21 +23,66 @@ function botonhome() {
     
     }
 
+    const continentes = ["AMERICA", "EUROPE", "ANTARTIDA", "AFRICA", "ASIA", "OCEANIA"];
+    function verificarContinenteEnArray(continente, arrayDeContinentes) {
+      return arrayDeContinentes.includes(continente)}
+   
+    function distanciaContinentes(){
+      
+      let primerContinente = prompt("Enter the continent you are at right now, (remember to write it down with upper letter case ):");
+      let segundoContinente = prompt("Enter the continent you are travelling to (remember to write it down with upper letter case):");
 
-    function mostrarCampoPais() {
-        var countryInput = document.getElementById("countryInput");
-        countryInput.style.display = "block";
-      }
-  
-      function enviarPais() {
-        var paisInput = document.getElementById("pais");
-        var pais = paisInput.value;
-        alert("If the information has been correctly submitted, you will be contacted soon");
-        paisInput.value = "";
-        
-        var countryInput = document.getElementById("countryInput");
-        countryInput.style.display = "none";
-      }
+      if (primerContinente !== null && primerContinente !== "" && segundoContinente !== null && segundoContinente !== "" && (verificarContinenteEnArray(primerContinente, continentes) && (verificarContinenteEnArray(segundoContinente, continentes)) && primerContinente!==segundoContinente)) {
+          if (primerContinente==="AMERICA" && segundoContinente==="EUROPE" || primerContinente==="EUROPE" && segundoContinente=="AMERICA"){
+           alert("You are close! Just 7715km approximately. Go check in despegar.com if you are looking for flight sales")}
+          else if( primerContinente==="AMERICA" && segundoContinente==="AFRICA" || primerContinente==="AFRICA" && segundoContinente=="AMERICA"){
+            alert("You are close! Just 9325 km approximately. Go check in despegar.com if you are looking for flight sales")}
+          else if (primerContinente==="AMERICA" && segundoContinente==="ASIA" || primerContinente==="ASIA" && segundoContinente=="AMERICA"){
+            alert("You are close! Just 8753 km approximately. Go check in despegar.com if you are looking for flight sales")}
+          else if ( primerContinente==="AMERICA" && segundoContinente==="OCEANIA" || primerContinente==="OCEANIA" && segundoContinente=="AMERICA"){
+          alert("You are close! Just 8753 km approximately. Go check in despegar.com if you are looking for flight sales")}
+          else if (primerContinente==="AMERICA" && segundoContinente==="ANTARTIDA" || primerContinente==="ANTARTIDA" && segundoContinente=="AMERICA"){
+            alert("You are close! Just 14600km km approximately. Go check in despegar.com if you are looking for flight sales")}
+
+          
+          else if (primerContinente==="EUROPE" && segundoContinente==="ANTARTIDA" || primerContinente==="ANTARTIDA" && segundoContinente=="EUROPE"){
+            alert("You are close! Just 15650 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="EUROPE" && segundoContinente==="AFRICA" || primerContinente==="AFRICA" && segundoContinente=="EUROPE"){
+              alert("You are close! Just 9000 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="EUROPE" && segundoContinente==="ASIA" || primerContinente==="ASIA" && segundoContinente=="EUROPE"){
+               alert("You are close! Just 7924 km approximately. Go check in despegar.com if you are looking for flight sales")}
+
+            else if (primerContinente==="EUROPE" && segundoContinente==="OCEANIA" || primerContinente==="OCEANIA" && segundoContinente=="EUROPE"){
+              alert("You are close! Just 15415 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="AFRICA" && segundoContinente==="ASIA" || primerContinente==="ASIA" && segundoContinente=="AFRICA"){
+                alert("You are close! Just 10860 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="AFRICA" && segundoContinente==="OCEANIA" || primerContinente==="OCEANIA" && segundoContinente=="AFRICA"){
+                  alert("You are close! Just 11184 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="AFRICA" && segundoContinente==="ANTARTIDA" || primerContinente==="ANTARTIDA" && segundoContinente=="AFRICA"){
+              alert("You are close! Just 9177 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="ASIA" && segundoContinente==="ANTARTIDA" || primerContinente==="ANTARTIDA" && segundoContinente=="ASIA"){
+              alert("You are close! Just 13121 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if (primerContinente==="ASIA" && segundoContinente==="OCEANIA" || primerContinente==="OCEANIA" && segundoContinente=="ASIA"){
+              alert("You are close! Just 7560 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            else if  (primerContinente==="ANTARTIDA" && segundoContinente==="OCEANIA" || primerContinente==="OCEANIA" && segundoContinente=="ANTARTIDA"){
+              alert("You are close! Just 6685 km approximately. Go check in despegar.com if you are looking for flight sales")}
+            }
+            
+            
+
+
+
+
+            
+          
+
+
+
+          
+      else {
+          // Al menos uno de los países no fue ingresado correctamente
+          alert("Please enter valid values for both countries. Remember to write them in upper letter case; your options are AMERICA, EUROPE, ANTARTIDA, AFRICA, ASIA and OCEANIA");
+      }}
     
       function mostrarCampo(inputId) {
         var inputContainer = document.getElementById(inputId);
